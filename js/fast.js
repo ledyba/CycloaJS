c7b12e3ea3a8e7da9d67c56ebf2ea96f56db8b3d
psi <psi@7io.org>
2012-08-16 21:16:47 +0900

    moved
    Signed-off-by: psi <ledyba@users.sourceforge.jp>

--- js/fast.js
+++ js/fast.js
@@ -1,28 +1,4 @@
 
-cycloa.AbstractPadFairy = function(){
-	this.A=0;
-	this.B=1;
-	this.SELECT=2;
-	this.START=3;
-	this.UP=4;
-	this.DOWN=5;
-	this.LEFT=6;
-	this.RIGHT=7;
-	this.TOTAL=8;
-	this.MASK_A=1;
-	this.MASK_B=2;
-	this.MASK_SELECT=4;
-	this.MASK_START=8;
-	this.MASK_UP=16;
-	this.MASK_DOWN=32;
-	this.MASK_LEFT=64;
-	this.MASK_RIGHT=128;
-	this.MASK_ALL=255;
-	this.isPressed = function(keyIdx){
-		return false;
-	}
-};
-
 /**
  * @constructor
  */
